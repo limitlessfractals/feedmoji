56153c0394386184c72e7ceb2926937fd5587deb
limitlessfractals <fanwuq@gmail.com>
2018-02-12 18:53:17 -0600

    SignIn.js

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import SignIn from './src/SignIn';
 
 export default class App extends React.Component {
+  state = {
+    loggedIn: false
+  }
+
+  renderView = () =>{
+    if(!this.state.loggedIn){
+      console.log("not signed in");
+      return(
+        <SignIn logIn={this.logIn}/>
+      )
+    }
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        {this.renderView()}
       </View>
     );
   }
@@ -15,7 +28,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
--- src/SignIn.js
+++ src/SignIn.js
@@ -3,12 +3,62 @@ import {View, Text, ImageBackground} from 'react-native';
 import {Form, Item, Label, Input, Button} from 'native-base';
 
 class SignIn extends React.Component{
+   state = {
+      email: "",
+      password: ""
+   }
+   login = () =>{
+      var email = this.state.email;
+      var password = this.state.password;
+
+      this.props.logIn(email, password);
+   }
    render(){
       return(
-         <View>
-            <Text>SignIn</Text>
+         <View style={{flex: 1}}>
+            <View style={styles.inputStyle}>
+               <Form>
+                  <Item floatingLabel>
+                     <Label>Email</Label>
+                     <Input
+                        autoCorrect={false}
+                        onChangeText={(email)=>this.setState({email})}
+                     />
+                  </Item>
+                  <Item floatingLabel>
+                     <Label>Password</Label>
+                     <Input
+                        autoCorrect={false}
+                        onChangeText={(password)=>this.setState({password})}
+                        secureTextEntry
+                     />
+                  </Item>
+               </Form>
+               <View style={{marginTop: 10}}>
+                  <Button
+                     primary
+                     block
+                     onPress={this.login}
+                  >
+                     <Text style={styles.buttonText}>Sign In/Sign Up</Text>
+                  </Button>
+               </View>
+            </View>
          </View>
       )
+   }
+}
+
+const styles = {
+   inputStyle: {
+      flex: 1,
+      flexDirection: 'column',
+      justifyContent: 'center',
+      margin: 10
+   },
+   buttonText: {
+      color: 'white',
+      fontWeight: 'bold'
    }
 }
 
